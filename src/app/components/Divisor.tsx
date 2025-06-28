cbfd05da6bad6a612353be44e95792744402bf54
willian <williangrios@yahoo.com.br>
2025-06-28 06:40:27 -0300

    feat: new component

---
+++ src/app/components/Divisor.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { envVariables } from '@/helpers/envVariables'
+
+interface DivisorProps {
+  inverse?: boolean
+}
+
+export default function Divisor({ inverse = false }: DivisorProps) {
+  const gradient = inverse
+    ? `linear-gradient(to top, ${envVariables.PRIMARY_COLOR_DARK}, ${envVariables.SECONDARY_COLOR_LIGHT})`
+    : `linear-gradient(to bottom, ${envVariables.PRIMARY_COLOR_DARK}, ${envVariables.SECONDARY_COLOR_LIGHT})`
+
+  return (
+    <div
+      className="w-full h-26"
+      style={{
+        background: gradient,
+      }}
+    />
+  )
+}
